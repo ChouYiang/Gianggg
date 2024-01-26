933e0c0ee2e4b78d490f17b5e65685bd41fe8726
Giang <giangnguyen0227@gmail.com>
2024-01-26 15:07:08 +0700

    abc

--- index.js
+++ index.js
@@ -1,105 +1,113 @@
-
 var express = require('express');
 var app = express();
-
-var { MongoClient } = require('mongodb');
-const mongoUrl = 'mongodb://localhost:27017'
-const mongodbClient = new MongoClient(mongoUrl)
-async function main() {
-  // Use connect method to connect to the server
-  const dbName = 'test'
-  await mongodbClient.connect();
-  console.log('Connected successfully to server');
-  const db = mongodbClient.db(dbName);
-  const collection = await db.collection('data');
-  // const data = await collection.find({_id: '65b207bb07901b369a4c6309'}).toArray();
-  const data = await collection.find().toArray();
-  console.log(data)
-  // the following code examples can be pasted here...
-
-  return 'done.';
-}
-main()  
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => mongodbClient.close());
-
-
-
-
-
-
 let PORT = 3000;
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-let products = [
-  { ID: 201, Name: 'Hoang', Detail: 'thạch Hạo', Price: 1.599 },
-  { ID: 3251, Name: 'Thiên', Detail: 'Thạch thôn', Price: 1.999 },
 
-]
+var { MongoClient } = require('mongodb');
+const mongoUrl = 'mongodb://localhost:27017'
+
+const mongodbClient = new MongoClient(mongoUrl)
+let database;
+async function main() {
+  mongodbClient.connect();
+  const dbName = 'test'
+  const db = mongodbClient.db(dbName);
+  database = db;
+  console.log('Kết nối thành công ! ');
+}
+main()
+  .catch(console.error)
 
 
-app.get('/', (request, response) => {
+
+app.get('/', async (request, response) => {
+  const collection = database.collection('data');
+  const products = await collection.find().toArray();
   response.render('index', { products })
 });
-app.post('/', (request, response) => {
-  products.push(request.body)
-  response.redirect('/')
+
+app.post('/', async (request, response) => {
+  try {
+    const collection = database.collection('data');
+    await collection.insertOne(request.body);
+    response.redirect('/');
+  } catch (error) {
+    response.status(500).send('Internal Server Error');
+  }
 });
+
+
 
 
 //Sửa
-app.get('/edit/:ID', (request, response) => {
+app.get('/edit/:ID',async(request, response) => {
+  const collection = database.collection('data');
+  const products = await collection.find().toArray();
   let productID = request.params.ID;
   let productUpdate = products.find(product => product.ID == productID);
   response.render('edit', { productUpdate });
 })
-app.post('/update/:ID', (request, response) => {
-  let productID = request.params.ID;
-  let productUpdate = products.find(product => product.ID == productID);
 
-  if (productUpdate) {
-    productUpdate.ID = request.body.idEdit;
-    productUpdate.Name = request.body.nameEdit;
-    productUpdate.Detail = request.body.DetailEdit;
-    productUpdate.Price = request.body.priceEdit;
+app.post('/update/:ID', async (request, response) => {
+  try {
+    const collection = database.collection('data');
+    const productID = request.params.ID;
+    const query = { ID: productID };
+    const existingProduct = await collection.findOne(query);
+
+    if (existingProduct) {
+      const updateData = {
+        $set: {
+          ID: request.body.idEdit,
+          Name: request.body.nameEdit,
+          Detail: request.body.DetailEdit,
+          Price: request.body.priceEdit
+        }
+      };
+
+      await collection.updateOne(query, updateData);
+    }
+    response.redirect('/');
+  } catch (error) {
+    response.status(500).send('Internal Server Error');
   }
-  response.redirect('/');
 });
-
 
 
 // Xóa
-app.get('/delete/:ID', (request, response) => {
+app.get('/delete/:ID', async(request, response) => {
+  const collection = database.collection('data');
+  const products = await collection.find().toArray();
   let productID = request.params.ID;
-  console.log('Deleting product with ID:', productID);
+  console.log('Xóa thành công ID:', productID);
   response.render('delete', { productID });
 });
 
-app.post('/delete/:ID', (request, response) => {
-  let productID = request.params.ID;
-  let productIndex = products.findIndex(product => product.ID == productID);
-  productIndex !== -1
-  products.splice(productIndex, 1);
-  response.redirect('/');
+app.post('/delete/:ID', async (request, response) => {
+  try {
+    const collection = database.collection('data');
+    const productID = request.params.ID;
+    await collection.deleteOne({ ID: productID });
+    response.redirect('/');
+  } catch (error) {
+    response.status(500).send('Internal Server Error');
+  }
 });
-
 
 // search
-app.get('/search', (request, response) => {
+app.get('/search', async(request, response) => {
+  const collection = database.collection('data');
+  const products = await collection.find().toArray();
   let searchTerm = request.query.term;
   let searchResults = products.filter(product =>
-    product.Name.toLowerCase().includes(searchTerm.toLowerCase())
+    product.Name.toLowerCase().includes(searchTerm.toLowerCase()),
   );
-
   response.render('search', { searchResults, searchTerm });
 });
-
-
-
 
 
 app.get('/canter', (request, response) => {
